c63d5569dccfd82cfb7579e605d3fb336348d310
Victor Sandoval <victor.sandoval@orbis.com.pe>
2019-02-27 16:33:31 -0500

    feat(app): se agrega estructura base

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store'
+import { App } from './view';
+
+const store = configureStore();
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
